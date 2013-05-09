350bb7c96186afba9f128fb396373d6e483f8997
Nolan Lawson <nolan.lawson@gmail.com>
2013-05-09 14:57:03 +0200

    wow, took me 1 hour to figure out a hello world rest service in Jersey.  Real time-saver.

--- src/main/java/com/nolanlawson/jnameconverter/server/JapaneseNameConverterService.java
+++ src/main/java/com/nolanlawson/jnameconverter/server/JapaneseNameConverterService.java
@@ -2,17 +2,17 @@ package com.nolanlawson.jnameconverter.server;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
 
 @Path("/convert")
 public class JapaneseNameConverterService {
 
     @GET
-    @Path("/{param}")
-    public Response getMsg(@PathParam("param") String msg) {
+    @Path("/")
+    public Response getMsg(@QueryParam("q") String query) {
 
-        String output = "Jersey say : " + msg;
+        String output = "Jersey say : " + query;
 
         return Response.status(200).entity(output).build();
 
